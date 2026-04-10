9f4d522f56c9071aa01cd179b28c9c82e918d5a9
Ángel Esteban <qsangel77@gmail.com>
2026-04-10 16:09:11 -0500

    V3 Agregada validación condicional de >> aprobación

--- programa.py
+++ programa.py
@@ -1,11 +1,10 @@
-print("--- Calculadora de Promedios V1 ---")
+print("--- Calculadora de Promedios V2 ---")
 
-nota1 = float(input("Ingrese nota 1: "))
-nota2 = float(input("Ingrese nota 2: "))
-nota3 = float(input("Ingrese nota 3: "))
-nota4 = float(input("Ingrese nota 4: "))
-nota5 = float(input("Ingrese nota 5: "))
+nota1 = float(input("Ingrese nota 1 (10%): "))
+nota2 = float(input("Ingrese nota 2 (20%): "))
+nota3 = float(input("Ingrese nota 3 (30%): "))
+nota4 = float(input("Ingrese nota 4 (40%): "))
 
-promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+promedio = (nota1 * 0.10) + (nota2 * 0.20) + (nota3 * 0.30) + (nota4 * 0.40)
 
-print(f"El promedio es: {promedio}")
+print(f"El promedio ponderado es: {promedio}")
