b501d4a94116f526397e44c7141f58cde0fe879e
Ángel Esteban <qsangel77@gmail.com>
2026-04-10 13:50:58 -0500

    V1: promedio simple

---
+++ programa.py
@@ -0,0 +1,11 @@
+print("--- Calculadora de Promedios V1 ---")
+
+nota1 = float(input("Ingrese nota 1: "))
+nota2 = float(input("Ingrese nota 2: "))
+nota3 = float(input("Ingrese nota 3: "))
+nota4 = float(input("Ingrese nota 4: "))
+nota5 = float(input("Ingrese nota 5: "))
+
+promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+
+print(f"El promedio es: {promedio}")
